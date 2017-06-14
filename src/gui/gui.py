bb52d907e0df43819d0a9d8607d1959a2f59d605
RafaelK <rafael_kuebler@yahoo.es>
2017-06-14 15:10:16 +0200

    Copy sample tkinter gui from tutorial.

---
+++ src/gui/gui.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from tkinter import ttk
+
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5) / 10000.0)
+    except ValueError:
+        pass
+
+
+root = Tk()
+root.title("Rainy")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+debug = StringVar()
+meters = StringVar()
+
+debug_output = Text(mainframe, width=40, height=10)
+debug_output.grid(column=1, row=1, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+# feet_entry.focus()
+root.bind('<Return>', calculate)
+
+root.mainloop()
